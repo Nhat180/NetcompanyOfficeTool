53a440a4201d6daa2173b6ab1e6fa69bb8039c50
Nhat180 <s3740975@rmit.edu.vn>
2022-04-29 21:30:55 +0700

    Create surveylist_screen.dart

---
+++ lib/screens/surveylist_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SurveyListScreen extends StatefulWidget {
+  const SurveyListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _State();
+}
+
+class _State extends State<SurveyListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Suggestion"),
+    );
+  }
+}
