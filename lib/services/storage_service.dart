61c1ad37a473448f92f312f251f81f1b0096ac98
Nhat180 <s3740975@rmit.edu.vn>
2022-06-19 11:56:20 +0700

    Create storage_service.dart

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:netcompany_office_tool/model/storage_item.dart';
+
+class StorageService {
+  final secureStorage = const FlutterSecureStorage();
+
+  AndroidOptions _getAndroidOptions() => const AndroidOptions(encryptedSharedPreferences: true);
+
+  Future<void> writeSecureData (StorageItem item) async {
+    await secureStorage.write(key: item.key, value: item.value, aOptions: _getAndroidOptions());
+  }
+
+  Future<String?> readSecureData (String key) async {
+    var readData = await secureStorage.read(key: key, aOptions: _getAndroidOptions());
+    return readData;
+  }
+
+  Future<void> deleteSecureData (StorageItem item) async {
+    await secureStorage.delete(key: item.key, aOptions: _getAndroidOptions());
+  }
+
+  Future<bool> isContainKey (String key) async {
+    var isContain = await secureStorage.containsKey(key: key, aOptions: _getAndroidOptions());
+    return isContain;
+  }
+}
