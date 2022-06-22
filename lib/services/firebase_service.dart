1ca6228e4a10bd6adb872a27a27658c413b97c28
Nhat180 <s3740975@rmit.edu.vn>
2022-06-22 18:31:36 +0700

    Create firebase_service.dart

---
+++ lib/services/firebase_service.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
+
+class FirebaseService {
+
+  Future<void> registerFirebase (String name, String password) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: name+"@gmail.com",
+          password: password
+      );
+      setUser(name);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        loginFirebase(name, password);
+      }
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
+    }
+  }
+
+  Future<void> setUser (String name) async {
+    final firestoreDB = FirebaseFirestore.instance;
+    await firestoreDB.collection("users").doc(name).set({
+      "username": name,
+      "isAdmin": false
+    });
+  }
+
+  Future<void> loginFirebase (String name, String password) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: name+"@gmail.com",
+          password: password
+      );
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        // ignore: avoid_print
+        print('No user found for that email.');
+      } else if (e.code == "wrong-password") {
+        // ignore: avoid_print
+        print('Wrong password');
+      }
+    }
+  }
+
+  Future<DateTime> getCrawlTimeStamp() async {
+    var collection = FirebaseFirestore.instance.collection("lunch");
+    var doc = await collection.doc("updateTime").get();
+    // String value = '';
+    DateTime dateTime = DateTime.now();
+    if (doc.exists) {
+      Map<String, dynamic>? data = doc.data();
+      Timestamp timestamp = data!['satDate'];
+      dateTime = timestamp.toDate();
+      // value = DateFormat('yyyy-MM-dd – kk:mm:ss').format(dateTime);
+    }
+    return dateTime;
+  }
+}
